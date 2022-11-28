2d8ab4878765d4fed5ce303956fea2fb120d897f
Petar <p.todorov212@gmail.com>
2022-11-29 01:19:11 +0200

    tests

--- test/test.js
+++ test/test.js
@@ -3,23 +3,85 @@ const { ethers } = require("hardhat");
 
 let Bridge;
 let bridge;
-let fee = 300000000000000000;
+let Token;
+let token;
+let fee = ethers.utils.parseEther("0.03");
 beforeEach(async () => {
   [owner, addr1, addr2] = await ethers.getSigners();
+  Bridge = await ethers.getContractFactory("Bridge");
+  bridge = await Bridge.deploy();
+  await bridge.deployed();
+  Token = await ethers.getContractFactory("WrappedToken");
+  token = await Token.deploy("BridgeToken", "BT", 18);
+  await token.deployed();
 });
 describe("Testing the bridge smart contract ... ", () => {
   it("Should allow everyone to lock tokens", async () => {
-    Bridge = await ethers.getContractFactory("Bridge");
-    bridge = await Bridge.deploy();
-    await bridge.deployed();
-    let Token = await ethers.getContractFactory("WrappedToken");
-    let token = await Token.deploy("BridgeToken", "BT", 18);
-    await token.deployed();
     let amount = 10;
     await token.mint(addr1.address, amount);
     await token.connect(addr1).approve(bridge.address, amount);
-    await bridge.lock(token.address, amount)({ value: fee });
+    await bridge.connect(addr1).lock(token.address, amount, { value: fee });
     let bridgeBalance = await token.balanceOf(bridge.address);
     expect(bridgeBalance).to.equal(amount);
+  });
+  it("should revert if the service fee is not paid", async () => {
+    let amount = 10;
+    await token.mint(addr1.address, amount);
+    await token.connect(addr1).approve(bridge.address, amount);
+    await expect(
+      bridge.connect(addr1).lock(token.address, amount)
+    ).to.be.revertedWithCustomError(bridge, "NoServiceFee");
+  });
+  it("Should send the service fee to the deployer of the Bridge contract", async () => {
+    let amount = 10;
+    let owner = await bridge.owner();
+    await token.mint(addr1.address, amount);
+    await token.connect(addr1).approve(bridge.address, amount);
+    let ownerBalance1 = await ethers.provider.getBalance(owner);
+    await bridge.connect(addr1).lock(token.address, amount, { value: fee });
+    let ownerBalance2 = await ethers.provider.getBalance(owner);
+    expect(ownerBalance2).to.equal(ownerBalance1.add(fee));
+  });
+  it("should revert if mint() is not called by the owner", async () => {
+    let amount = 10;
+    let name = await token.name();
+    let symbol = await token.symbol();
+    let decimals = await token.decimals();
+    let tokenAddress = token.address;
+    await token.mint(addr1.address, amount);
+    await token.connect(addr1).approve(bridge.address, amount);
+    await bridge.connect(addr1).lock(token.address, amount, { value: fee });
+    await expect(
+      bridge
+        .connect(addr1)
+        .mint(name, symbol, decimals, amount, addr1.address, tokenAddress)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+  it("should revert if unlock() is not called by the owner", async () => {
+    let amount = 10;
+    await token.mint(addr1.address, amount);
+    await token.connect(addr1).approve(bridge.address, amount);
+    await bridge.connect(addr1).lock(token.address, amount, { value: fee });
+    await expect(
+      bridge.connect(addr1).unlock(token.address, amount, addr1.address)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+  it("Should revert if the user attempts to bridge back an amount, gretaer than his balance", async () => {
+    let amount = 10;
+    await token.mint(addr1.address, amount);
+    await token.connect(addr1).approve(bridge.address, amount);
+    await bridge.connect(addr1).lock(token.address, amount, { value: fee });
+    await expect(
+      bridge.connect(addr1).burn(token.address, amount * 2, { value: fee })
+    ).to.be.revertedWithCustomError(bridge, "InsufficientBalance");
+  });
+  it("Should revert if the user attempts to bridge back an amount and does not pay the service fee", async () => {
+    let amount = 10;
+    await token.mint(addr1.address, amount);
+    await token.connect(addr1).approve(bridge.address, amount);
+    await bridge.connect(addr1).lock(token.address, amount, { value: fee });
+    await expect(
+      bridge.connect(addr1).burn(token.address, amount * 2)
+    ).to.be.revertedWithCustomError(bridge, "NoServiceFee");
   });
 });
