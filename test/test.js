c2b286d36da99e24a89eb3fa48c8c0ab1eb66d07
Petar <p.todorov212@gmail.com>
2022-11-29 13:57:25 +0200

    finished the static tests

--- test/test.js
+++ test/test.js
@@ -57,6 +57,41 @@ describe("Testing the bridge smart contract ... ", () => {
         .mint(name, symbol, decimals, amount, addr1.address, tokenAddress)
     ).to.be.revertedWith("Ownable: caller is not the owner");
   });
+  it("Should emit a Mint event when mint () is called", async () => {
+    let amount = 10;
+    let name = await token.name();
+    let symbol = await token.symbol();
+    let decimals = await token.decimals();
+    let tokenAddress = token.address;
+    await token.mint(addr1.address, amount);
+    await token.connect(addr1).approve(bridge.address, amount);
+    await bridge.connect(addr1).lock(token.address, amount, { value: fee });
+    await expect(
+      bridge.mint(name, symbol, decimals, amount, addr1.address, tokenAddress)
+    ).to.emit(bridge, "Mint");
+  });
+  it(" Should generate an address for the newly created wrapped token", async () => {
+    let capturedValue;
+    const captureValue = (value) => {
+      capturedValue = value;
+      return true;
+    };
+    let amount = 10;
+    let name = await token.name();
+    let symbol = await token.symbol();
+    let decimals = await token.decimals();
+    let tokenAddress = token.address;
+    await token.mint(addr1.address, amount);
+    await token.connect(addr1).approve(bridge.address, amount);
+    await bridge.connect(addr1).lock(token.address, amount, { value: fee });
+    await expect(
+      bridge.mint(name, symbol, decimals, amount, addr1.address, tokenAddress)
+    )
+      .to.emit(bridge, "Mint")
+      .withArgs(captureValue, amount, addr1.address);
+    assert(capturedValue != 0x0);
+    assert((await bridge.nativeToWrapped(capturedValue)) == tokenAddress);
+  });
   it("should revert if unlock() is not called by the owner", async () => {
     let amount = 10;
     await token.mint(addr1.address, amount);
@@ -65,6 +100,16 @@ describe("Testing the bridge smart contract ... ", () => {
     await expect(
       bridge.connect(addr1).unlock(token.address, amount, addr1.address)
     ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+  it("Should emit a Unlock event when unlock () is called", async () => {
+    let amount = 10;
+    await token.mint(addr1.address, amount);
+    await token.connect(addr1).approve(bridge.address, amount);
+    await bridge.connect(addr1).lock(token.address, amount, { value: fee });
+    await expect(bridge.unlock(token.address, amount, addr1.address)).to.emit(
+      bridge,
+      "Unlock"
+    );
   });
   it("Should revert if the user attempts to bridge back an amount, gretaer than his balance", async () => {
     let amount = 10;
@@ -83,5 +128,30 @@ describe("Testing the bridge smart contract ... ", () => {
     await expect(
       bridge.connect(addr1).burn(token.address, amount * 2)
     ).to.be.revertedWithCustomError(bridge, "NoServiceFee");
+  });
+  it("Should send the service fee to the deployer of the Bridge contract when burn() is called", async () => {
+    let capturedValue;
+    const captureValue = (value) => {
+      capturedValue = value;
+      return true;
+    };
+    let amount = 10;
+    let name = await token.name();
+    let symbol = await token.symbol();
+    let decimals = await token.decimals();
+    let tokenAddress = token.address;
+    let owner = await bridge.owner();
+    await token.mint(addr1.address, amount);
+    await token.connect(addr1).approve(bridge.address, amount);
+    await bridge.connect(addr1).lock(token.address, amount, { value: fee });
+    await expect(
+      bridge.mint(name, symbol, decimals, amount, addr1.address, tokenAddress)
+    )
+      .to.emit(bridge, "Mint")
+      .withArgs(captureValue, amount, addr1.address);
+    let ownerBalance1 = await ethers.provider.getBalance(owner);
+    await bridge.connect(addr1).burn(capturedValue, amount, { value: fee });
+    let ownerBalance2 = await ethers.provider.getBalance(owner);
+    expect(ownerBalance2).to.equal(ownerBalance1.add(fee));
   });
 });
